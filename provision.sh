c84af01c2ae634e4fa375d8d755e34b5d3df78ef
BA-cybersec <102294072+BA-cybersec@users.noreply.github.com>
2022-06-14 22:48:50 +0100

    Added new files

--- provision.sh
+++ provision.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
-#update
+#update & upgrade
 sudo apt-get update -y
 
-#upgrade
 sudo apt-get upgrade -y
 
-#install nginx
+# install nginx
 sudo apt-get install nginx -y
 
-#start nginx
-sudo systemctl start nginx
-
-#enable nginx
+# enable nginx
 sudo systemctl enable nginx
 
-#download v6
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# start nginx
+sudo systemctl start nginx
 
-#install nodejs
+# install nodejs  version 6
+sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# install nodejs 
 sudo apt-get install nodejs -y
+sudo apt-get install python-software-properties -y
 
-#install pm2
+cd app/app/
+
+# install pm2
 sudo npm install pm2 -g
-
-#download python stuff
-sudo apt-get install python-software-properties
-
-#redirect to app folder
-cd /app/app/app/
-
-#npm install and d to detach
-npm install -d
 
 #start npm
 npm start
-
